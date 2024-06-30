260630c1128fa707278224a94cefc933c86f06d3
Mutasem <mutasemshboul14@gmail.com>
2024-06-30 13:58:24 +0300

    Solve Task 5

---
+++ Task5/DateDifferenceCalculator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    public class DateDifferenceCalculator
+    {
+
+        public static void GetDifference(DateTime startDate, DateTime endDate)
+        {
+            if (endDate < startDate)
+            {
+                DateTime temp = startDate;
+                startDate = endDate;
+                endDate = temp;
+            }
+
+            int years = endDate.Year - startDate.Year;
+            int months = endDate.Month - startDate.Month;
+            int days = endDate.Day - startDate.Day;
+
+            if (days < 0)
+            {
+                months--;
+                days += DateTime.DaysInMonth(startDate.Year, startDate.Month);
+            }
+
+            if (months < 0)
+            {
+                years--;
+                months += 12;
+            }
+
+            Console.WriteLine($"Difference: {years} years, {months} months, {days} days.");
+        }
+    }
+}
---
+++ Task5/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Globalization;
+
+namespace Task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            /*   1   */
+            MyClass myClass = new MyClass();
+
+
+            /*   2   */
+            static void DisplayGreeting(string name)
+            {
+                Console.WriteLine($"Hello All, I am {name}");
+            }
+            DisplayGreeting("Scott");
+
+            /*   3   */
+
+
+            static void factorial(int n)
+            {
+                int c = 1;
+                for (int i = n; i >= 1; i++)
+                {
+                    c *= i;
+                    c--;
+                }
+            }
+
+            factorial(4);
+
+            /*   4   */
+
+            int[] array = { 11, -2, 4, 35, 0, 8, -9 };
+            Array.Sort(array);
+            foreach (int i in array)
+            {
+                Console.WriteLine(i);
+            }
+
+
+            /*   5   */
+
+            DateTime date1 = new DateTime(1981, 11, 03);
+            DateTime date2 = new DateTime(2013, 09, 04);
+            DateDifferenceCalculator.GetDifference(date1, date2);
+
+
+
+            /*   6   */
+
+            string dateString = "12-08-2004";
+            string format = "dd-MM-yyyy";
+
+            DateTime dateValue = DateTime.ParseExact(dateString, format, CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Converted Date: " + dateValue.ToString("yyyy-MM-dd"));
+        }
+    }
+}
